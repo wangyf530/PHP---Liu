8c2cd1372d7950208a200146bd61df3144ef3773
Vanessa laptop <wangyf223@gmail.com>
2024-10-28 22:20:39 +0800

    完善萬年曆

--- calendar.php
+++ calendar.php
@@ -7,9 +7,11 @@
     <style>
     table {
         border-collapse:collapse;
+        margin:auto;
+        width:300px;
     }
     td,th{
-        padding:5px 10px;
+        /* padding:5px 10px; */
         text-align: center;
         border:1px solid #999;
     }
@@ -28,6 +30,25 @@
         color: white;
         font-weight: bolder;
     }
+
+    .clicks{
+        font-size:18px;
+        border:2px solid black;
+        margin: auto;
+        text-align: center;
+        width:300px;
+        background: lightgrey;
+    }
+
+    a{
+        padding:3px;
+    }
+    a:hover{
+        background:black;
+        color:white;
+    }
+
+
     </style>
 </head>
 
@@ -43,6 +64,7 @@
     <li>萬年曆都在同一個頁面同一個檔案</li>
 </ul>
 <?php
+    // 如果有參數就按照參數來 沒有的話就用當前年月份
     if (isset($_GET['month'])) {
         $month = $_GET['month'];
     } else {
@@ -53,81 +75,88 @@
     } else {
         $year = date("Y");
     }
-
-    // 當月第一天
-    $firstDay = strtotime("$year-$month-1");
-    // $firstDayTime = strtotime($firstDay);
-    // 當月第一天是週幾
-    $firstDayWeek= date("w",$firstDay);
-    // $firstDayWeek = date("w",strtotime(date("Y-m-1")));
-    // 這個月有幾天
-    $daysInMonth = date("t",$firstDay);
-    // 這個月的最後一天
-    $lastDayInMonth = strtotime(date("Y-$month-$daysInMonth"));
-
-    $days=[];
-    for ($i=0; $i <42; $i++) { 
-        // 算出第一天在第幾格
-        $diff=$i-$firstDayWeek;
-        $days[]=date("Y-m-d",strtotime("$diff days",$firstDay));
-    }
    
-
+    // 一月份的上一個月會是去年的12月
     if ($month-1 <1) {
         $prevMonth=12;
         $prevYear = $year-1;
+    // 其他月份的上一個月就是同一年的前一個月
     } else {
         $prevMonth = $month-1;
         $prevYear = $year;
     } 
-    
+    // 12月份的下一個月會是明年的1月
     if ($month+1 >12) {
         $nextMonth = 1;
         $nextYear = $year+1;
+    // 其他月份的下一個月就是同一年的後一個月
     } else {
         $nextMonth = $month +1;
         $nextYear = $year;
     }
 ?>
+<div class='clicks'>
+    <!-- 連結前往去年、上一個月、今天、下一個月、以及明年 -->
+    <a href="calendar.php?year=<?=$year-1;?>&month=<?=$month;?>"> 去年 </a>
+    <a href="calendar.php?year=<?=$prevYear;?>&month=<?=$prevMonth;?>"> 上一個月 </a>
+    <a href="calendar.php?year=<?=date("Y");?>&month=<?=date('m');?>"> 今天 </a>
+    <a href="calendar.php?year=<?=$nextYear;?>&month=<?=$nextMonth;?>"> 下一個月 </a>
+    <a href="calendar.php?year=<?=$year+1;?>&month=<?=$month;?>"> 明年 </a>
+</div>
 
-<a href="calendar.php?year=<?=$year-1;?>&month=<?=$month;?>"> 去年 </a>
-<a href="calendar.php?year=<?=$prevYear;?>&month=<?=$prevMonth;?>"> 上一個月 </a>
-<a href="calendar.php?year=<?=date("y");?>&month=<?=date('m');?>"> 今天 </a>
-<a href="calendar.php?year=<?=$nextYear;?>&month=<?=$nextMonth;?>"> 下一個月 </a>
-<a href="calendar.php?year=<?=$year+1;?>&month=<?=$month;?>"> 明年 </a>
-
+<!-- 萬年曆的表格 -->
 <table>
 <?php
-// 印月份 
-$month = date('Y年-m月',$firstDay);
-echo "<tr> <td colspan=8>".$month."</td>";
-// 印週幾
-$day=[' ','日','一','二','三','四','五','六'];
-echo "<tr>";
-foreach ($day as $key) {
-    if ($key=='日' || $key =='六'){
-        echo "<td class='holiday'> $key </td>";
-    } else {
-    echo "<td> $key </td>";
+    // 當月第一天
+    $firstInMonth = "$year-$month-1";
+    $firstDayTime = strtotime($firstInMonth);
+    // 當月第一天是週幾
+    $firstDayWeek= date("w",$firstDayTime);
+    // $firstDayWeek = date("w",strtotime(date("Y-m-1")));
+
+
+    // 印月份 
+    $month = date('Y年-m月',$firstDayTime);
+    echo "<tr> <td colspan=8>".$month."</td>";
+    // 印週幾
+    $day=[' ','日','一','二','三','四','五','六'];
+    echo "<tr>";
+    foreach ($day as $key) {
+        if ($key=='日' || $key =='六'){
+            echo "<td class='holiday'> $key </td>";
+        } else {
+        echo "<td> $key </td>";
+        }
     }
-}
-echo "</tr>";
+    echo "</tr>";
 
 
-// 印日期
-    foreach ($days as $day) {
-        // 只get後面的月日
-        $format = explode("-",$day)[2];
-        // Numeric representation of the day of the week(0-6)
-        $w = date("w",strtotime($day));
-        // 印出的日期月份是否與現在的月份相同
-        $theMonth = date("m",$theDayTime)==date("m")?"":"grey-text";
-        // 印出的日期月份是否是今天
-        $isToday = date("Y-m-d",$theDayTime)==date("Y-m-d")?"today":"";
-        // 印出的日期月份是否是周末
-        $isHoliday = ($w == 0 || $w == 6)?"holiday":"";
-        $w=date("w",$theDayTime);
-        echo "<td class = '$isHoliday $theMonth $isToday'>";
+    // 印日期 
+    for ($i=0; $i<6; $i++) { 
+        echo "<tr>";
+        // 印第幾周
+        echo "<td>";
+        echo $i+1;
+        echo "</td>";
+
+        for ($j=0; $j<7; $j++) { 
+            // 幾號 當月第一天會是0
+            $cell = $i*7 + $j - $firstDayWeek;
+            // 當月第一天的日期
+            $theDayTime = strtotime("$cell days".$firstInMonth);
+
+            // 不是當月的話字會變灰
+            $theMonth = (date("m",$theDayTime) == date("m",$firstDayTime))?'':'grey-text';
+            // 是今天的話就高亮
+            $isToday = (date("Y-m-d",$theDayTime) == date("Y-m-d"))?'today':'';
+            // 判斷當天是否是週末
+            $w = date("w",$theDayTime);
+            $isHoliday = ($w==6 || $w==0)?'holiday':'';
+            echo "<td class = '$theMonth $isToday $isHoliday'>";
+            echo date('d',$theDayTime);
+            echo "</td>";
+        }
+        echo "</tr>";
     }
 
 ?>
